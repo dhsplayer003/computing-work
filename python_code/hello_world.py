4918cb6e0ebb196cb637deb88dd04432ffde4000
dhsplayer003 <69620850+dhsplayer003@users.noreply.github.com>
2020-09-12 23:26:15 +0800

    Create hello_world.py

---
+++ python_code/hello_world.py
@@ -0,0 +1,7 @@
+
+#This code should scroll the words 'Hello, World!' across the micro:bit. 
+#But there is an error!
+#Can you figure out how to fix it? ;)
+
+from microbit import *
+display.scroll("Hello, World!)
